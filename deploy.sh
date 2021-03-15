e717a59b31c89300b03403fdd328f6b597d48fb2
Alvaro Lopez Medina <alopezme@redhat.com>
2021-03-16 00:21:03 +0100

    Change config

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ echo -ne "\n\nWaiting for pods ready..."
 while [[ $(oc get pods -l deploymentconfig=postgresql -n $PROJECT_NAME -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
 
 # Deploy Etherpad 
-oc process -f etherpad-template.yaml -p DB_TYPE=postgres -p DB_HOST=postgresql -p DB_PORT=5432 -p DB_DATABASE=etherpad -p DB_USER=ether -p DB_PASS=ether -p ETHERPAD_IMAGE=quay.io/wkulhanek/etherpad:1.8.4 -p ADMIN_PASSWORD=secret | oc apply -f -
+oc process -f ./etherpad-template.yaml -p DB_TYPE=postgres -p DB_HOST=postgresql -p DB_PORT=5432 -p DB_DATABASE=etherpad -p DB_USER=ether -p DB_PASS=ether -p ETHERPAD_IMAGE=quay.io/wkulhanek/etherpad:1.8.4 -p ADMIN_PASSWORD=secret | oc apply -f -
 
 echo -ne "\n\nWaiting for pods ready..."
 while [[ $(oc get pods -l app=etherpad -n $PROJECT_NAME -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
